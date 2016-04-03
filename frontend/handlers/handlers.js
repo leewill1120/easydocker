1f0a4aec6377f9c39c9b9694bfe99da36a013552
leewill1120 <leewill1120@gmail.com>
2016-04-03 08:59:55 +0800

    add pageInfo

--- frontend/handlers/handlers.js
+++ frontend/handlers/handlers.js
@@ -13,16 +13,8 @@ exports.image = function(req, res){
 
 exports.containerlist = function(req, res){
 	var list = [];
-	for(var i = 1; i < 100; i++){
+	for(var i = 1; i < 200 ; i++){
 		list.push({'sn':i, 'name':'cdm' + (i < 100 ? (i < 10 ? '00' + i : '0' + i ): i), 'image':'cdm:D115SP93', 'ipAddress':'207.207.90.' + i, 'status':{'stat':'success', 'description':'运行中'}, 'created':'2016-1-25 2:25:16'});
 	}
-
-	var sendList = [];
-	for (var i = parseInt(req.query.start) - 1; i < parseInt(req.query.end) - 1; i++) {
-		if(i >= list.length){
-			break;
-		}
-		sendList.push(list[i]);
-	}
-	res.send(sendList);
+	res.send({'list':list});
 }
--- frontend/public/javascripts/container.js
+++ frontend/public/javascripts/container.js
@@ -1,5 +1,8 @@
 var totalPage = 0;
 var currentPage = 0;
+var containerList = [];
+var searchList = [];
+var serachMode = false;
 
 function test(){
 	alert('This is a test');
@@ -23,6 +26,14 @@ function stopContainer(name){
 
 function removeContainer(name){
 	alert('removeContainer');
+}
+
+function getTableCap(){
+	var count = Math.floor(($(window).height() - 150 ) / 51);
+	if(count < 1){
+		count = 1;
+	}
+	return count;
 }
 
 function addItemToTable(tableId, item){
@@ -84,44 +95,86 @@ function addItemToTable(tableId, item){
 	li = $('<li role="presentition"></li>').appendTo(ul);
 	a = $('<a role="menuitem" href="javascript:removeContainer(\'' + item.name + '\');"></a>').appendTo(li);
 	span = $('<span class="glyphicon glyphicon-remove">删除容器</span>').appendTo(a);
-
 }
 
-function getContainerList(start, end){
-	$.get('/containerlist?start=' + start + '&end='+ end, function(data, status){
+function emptyTable(table_body_id){
+	$('#' + table_body_id).empty();
+}
+
+function getContainerList(callback){
+	$.get('/containerlist', function(data, status){
 		if('success' == status){
-			for (var i = 0; i < data.length; i++) {
-				addItemToTable('id_table_body', data[i]);
+			containerList = data.list;
+			var cap = getTableCap();
+			totalPage = Math.ceil(data.list.length / cap);
+			for (var i = 0; i < cap; i++) {
+				addItemToTable('id_table_body', containerList[i]);
 			}
-			totalPage = data.length;
-			currentPage = 1;
-			printPageInfo(currentPage, totalPage);
+			if(callback != undefined){
+				callback();
+			}
 		}else{
 			console.log(status);
 		}
 	});
 }
 
+function goToPage(page){
+	if(currentPage == page){
+		return;
+	}else{
+		var targetList = [];
+		if(serachMode == true){
+			targetList = searchList;
+			totalPage = Math.ceil(searchList.length / getTableCap());
+		}else{
+			targetList = containerList;
+			totalPage = Math.ceil(containerList.length / getTableCap());
+		}
+		
+		emptyTable('id_table_body');
+		currentPage = page;
+		var cap = getTableCap();
+		for (var i = 0; i < cap; i++) {
+			if((i + (currentPage - 1) * cap) < targetList.length)
+			{
+				addItemToTable('id_table_body', targetList[i + (currentPage - 1) * cap]);
+			}
+		}
+		var children = $('#id_ul_page').children();
+		for (var i = 1; i < children.length - 1; i++) {
+			if( page == parseInt($($(children[i]).children()[0]).text())){
+				$(children[i]).addClass('active');
+			}else{
+				$(children[i]).removeClass('active');
+			}
+			if(page == totalPage){
+				$($(children[i]).children()[0]).text( totalPage - 10 + i);
+				$($(children[i]).children()[0]).attr('href', 'javascript:goToPage(' + (totalPage - 10 + i) + ');');
+			}
+		}
+		updatePageInfoPos();
+	}
+}
+
 function printPageInfo(current, total){
 	var li = $('<li></li>').appendTo($('#id_ul_page'));
 	var a = $('<a href="javascript:prePage();">&laquo</a>').appendTo(li);
-	var pos =  0;
 
 	if(total <= 10){
 		for (var i = 1; i <= total; i++) {
 			li = $('<li></li>').appendTo($('#id_ul_page'));
-			a = $('<a>' + i + '</a>').appendTo(li);
+			a = $('<a href="javascript:goToPage(' + i + ');">' + i + '</a>').appendTo(li);
 			if(current == i){
 				li.addClass('active');
 			}else{
 				li.removeClass('active');
 			}
 		}
-		pos = ($(window).width() - (total + 2) * a.innerWidth()) / 2;
 	}else{
 		for (var i = current; i < current + 8; i++) {
 			li = $('<li></li>').appendTo($('#id_ul_page'));
-			a = $('<a>' + i + '</a>').appendTo(li);
+			a = $('<a href="javascript:goToPage(' + i + ');">' + i + '</a>').appendTo(li);
 			if(current == i){
 				li.addClass('active');
 			}else{
@@ -131,21 +184,53 @@ function printPageInfo(current, total){
 		li = $('<li></li>').appendTo($('#id_ul_page'));
 		a = $('<a>...</a>').appendTo(li);
 		li = $('<li></li>').appendTo($('#id_ul_page'));
-		a = $('<a>' + total + '</a>').appendTo(li);
-		pos = ($(window).width() - 10 * a.innerWidth()) / 2;
+		a = $('<a href="javascript:goToPage(' + total + ');">' + total + '</a>').appendTo(li);
 	}
 
 	li = $('<li></li>').appendTo($('#id_ul_page'));
 	$('<a href="javascript:nextPage();">&raquo</a>').appendTo(li);
 
+	updatePageInfoPos();
+}
+
+function updatePageInfoPos(){
+	var w = 0;
+	var list = $('#id_ul_page').children();
+	for (var i = 0; i < list.length; i++) {
+		w += $($(list[i]).children()[0]).innerWidth();
+	}
+	pos = ($(window).width() - w) / 2;
+
 	$('#id_ul_page').css({'margin-left':pos});
 }
 
 function nextPage(){
+	var targetList = [];
+	if(serachMode == true){
+		targetList = searchList;
+		totalPage = Math.ceil(searchList.length / getTableCap());
+	}else{
+		targetList = containerList;
+		totalPage = Math.ceil(containerList.length / getTableCap());
+	}
+
 	if(currentPage < totalPage){
+		emptyTable('id_table_body');
 		currentPage++;
+
+		var cap = getTableCap();
+		for (var i = 0; i < cap; i++) {
+			if((i + (currentPage - 1) * cap) < targetList.length)
+			{
+				addItemToTable('id_table_body', targetList[i + (currentPage - 1) * cap]);
+			}
+		}
+
 		var children = $('#id_ul_page').children();
-		for (var i = 0; i < children.length; i++) {
+		if(currentPage == 1 + parseInt($($(children[children.length - 4]).children()[0]).text())){
+			rightPageBar();
+		}
+		for (var i = 1; i < children.length - 1; i++) {
 			if( currentPage == parseInt($($(children[i]).children()[0]).text())){
 				$(children[i]).addClass('active');
 			}else{
@@ -156,10 +241,30 @@ function nextPage(){
 }
 
 function prePage(){
+	var targetList = [];
+	if(serachMode == true){
+		targetList = searchList;
+	}else{
+		targetList = containerList;
+	}
+
 	if(currentPage > 1){
+		emptyTable('id_table_body');
 		currentPage--;
+
+		var cap = getTableCap();
+		for (var i = 0; i < cap; i++) {
+			if((i + (currentPage - 1) * cap) < targetList.length)
+			{
+				addItemToTable('id_table_body', targetList[i + (currentPage - 1) * cap]);
+			}
+		}
+
 		var children = $('#id_ul_page').children();
-		for (var i = 0; i < children.length; i++) {
+		if(currentPage + 1 == parseInt($($(children[1]).children()[0]).text())){
+			leftPageBar();
+		}
+		for (var i = 1; i < children.length - 1; i++) {
 			if( currentPage == parseInt($($(children[i]).children()[0]).text())){
 				$(children[i]).addClass('active');
 			}else{
@@ -169,14 +274,89 @@ function prePage(){
 	}
 }
 
-$(document).ready(function(){
-	var count = Math.floor(($(window).height() - 150) / 51);
+function leftPageBar(){
+	var children = $('#id_ul_page').children();
+	if(parseInt($($(children[1]).children()[0]).text()) == 1){
+		return;
+	}else{
+		for (var i = 1; i < children.length - 2; i++) {
+			if( (i == children.length - 2 - 1) && (parseInt($($(children[i - 1]).children()[0]).text()) + 2 != parseInt($($(children[i + 1]).children()[0]).text()) )){
+				$($(children[i]).children()[0]).text('...');
+				$($(children[i]).children()[0]).removeAttr("href");
+				continue;
+			}
+			if(isNaN($($(children[i]).children()[0]).text())){
+				continue;
+			}
+			var num = parseInt($($(children[i]).children()[0]).text());
+			$($(children[i]).children()[0]).text(num - 1);
+			$($(children[i]).children()[0]).attr("href", "javascript:goToPage('" + (num - 1) + "');");
+			updatePageInfoPos();
+		}
+	}
+}
 
+function rightPageBar(){
+	var children = $('#id_ul_page').children();
+	if( ! isNaN($(children[children.length - 3]).text()) ){
+		return;
+	}else{
+		for (var i = 1; i < children.length - 2; i++) {
+			if( (i == children.length - 2 - 1) && (parseInt($($(children[i - 1]).children()[0]).text()) + 2 == parseInt($($(children[i + 1]).children()[0]).text()) )){
+				$($(children[i]).children()[0]).text(totalPage - 1);
+				$($(children[i]).children()[0]).attr("href", "javascript:goToPage('" + (totalPage - 1) + "');");
+				continue;
+			}
+			if(isNaN($($(children[i]).children()[0]).text())){
+				continue;
+			}
+			var num = parseInt($($(children[i]).children()[0]).text());
+			$($(children[i]).children()[0]).text(num + 1);
+			$($(children[i]).children()[0]).attr("href", "javascript:goToPage('" + (num + 1) + "');");
+			updatePageInfoPos();
+		}
+	}
+}
+
+function search(keyword){
+	searchList = [];
+	if(keyword == ''){
+		serachMode = false;
+	}else{
+		serachMode = true;
+	}
+	
+	for (var i = 0; i < containerList.length; i++) {
+		if( -1 != containerList[i].name.toLowerCase().indexOf(keyword.toLowerCase()) || -1 != containerList[i].ipAddress.toLowerCase().indexOf(keyword.toLowerCase()))
+		searchList.push(containerList[i]);
+	}
+
+	currentPage = 1;
+	var cap = getTableCap();
+	emptyTable('id_table_body');
+	for (var i = 0; i < cap; i++) {
+		if((i + (currentPage - 1) * cap) < searchList.length)
+		{
+			addItemToTable('id_table_body', searchList[i + (currentPage - 1) * cap]);
+		}
+	}
+	$('#id_ul_page').empty();
+	printPageInfo(currentPage, Math.ceil(searchList.length / cap));
+}
+
+$(document).ready(function(){
 	$('#id_btn_refresh').click(function(){
 		window.location.reload();
 	});
 
-	var start = 1;
-	var end = start + count;
-	getContainerList(start, end);
+	$('#id_search_input').keyup(function(){
+		search($('#id_search_input').val());
+	});
+
+	currentPage = 1;
+	getContainerList(function(){
+		if(0 < totalPage){
+			printPageInfo(currentPage, totalPage);
+		}
+	});
 });
